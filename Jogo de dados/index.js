7fd2dd29ff8a5d1d7c75f7884ff3efcbbb7298e1
luizdo085 <luizmatheusbezerralmb@gmail.com>
2025-08-28 11:47:18 -0300

    Adicionando alguns ajustes e comentários.
    Ajustei a entrada para começar os dois dados com 6 números e adicionei mais comentários nós códigos.

--- Jogo de dados/index.js
+++ Jogo de dados/index.js
@@ -4,12 +4,14 @@ const maximo = 5;//gerando dois números aleatorio até 6
 const numeroInteiro = Math.floor(Math.random()*maximo) + 1;
 const numeroInteiro2 = Math.floor(Math.random()*maximo) + 1;
 
+//Selecionando a imagem do dado com os números aleatorios
 document.querySelectorAll(".dice")[0].lastElementChild.setAttribute("src", "./images/dice"+numeroInteiro+".png");
 document.querySelectorAll(".dice")[1].lastElementChild.setAttribute("src", "./images/dice"+numeroInteiro2+".png");
 
 if(estadoSessao === '1'){
     // Altera o estado para '1' para o próximo recarregamento
     sessionStorage.setItem('estado', '1');
+    //If pra dizer qual player foi o vencedor
     if(numeroInteiro>numeroInteiro2){
         document.querySelector(".h2-informacao").innerHTML = "Player 1 is winner!"
     }
@@ -24,4 +26,8 @@ if(estadoSessao === '1'){
 else {
     // Altera o estado para '1' para o próximo recarregamento
     sessionStorage.setItem('estado', '1');
+
+    //deixar os dados com 6 numeros
+    document.querySelectorAll(".dice")[0].lastElementChild.setAttribute("src", "./images/dice6.png");
+document.querySelectorAll(".dice")[1].lastElementChild.setAttribute("src", "./images/dice6.png");
 }
